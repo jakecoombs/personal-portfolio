d1e9cf64f60e03bb3ce6bd12af95156b16869f05
jcoombs235 <jakecoombs235@gmail.com>
2021-07-19 00:04:51 +0100

    Add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -35,6 +35,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
           <meta charSet="utf-8" />
         </Head>
         <body>
