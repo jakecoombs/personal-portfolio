edc722a78aedb0367406551c39be864484fd00e7
jcoombs235 <jakecoombs235@gmail.com>
2021-07-19 23:23:52 +0100

    use next/head

--- src/pages/api-test-suite.tsx
+++ src/pages/api-test-suite.tsx
@@ -1,10 +1,11 @@
 import { ArrowLink } from "../components/molecules/ArrowLink";
+import Head from "next/head";
 
 const ApiTestPage = () => (
   <>
-    <head>
+    <Head>
       <title>API | Jake Coombs</title>
-    </head>
+    </Head>
     <h1>Api Test Suite</h1>
     <h3>
       This is an API Test Suite built using GraphQL and deployed using Heroku.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,13 @@ import { Introduction } from "../components/organisms/Introduction/Introduction"
 import { MainHeader } from "../components/organisms/MainHeader/MainHeader";
 import { Projects } from "../components/organisms/Projects/Projects";
 import { Technologies } from "../components/organisms/Technologies/Technologies";
+import Head from "next/head";
 
 const IndexPage = () => (
   <>
-    <head>
+    <Head>
       <title>Jake Coombs</title>
-    </head>
+    </Head>
     <MainHeader />
     <Introduction />
     <Projects />
